e76b30f0b7fcaf7abbd9a9ef922e046f6e2259a3
Naveed Nadjmabadi <naveedn@users.noreply.github.com>
2017-07-13 20:00:08 -0400

    Adds options to constraints

--- constraints.js
+++ constraints.js
@@ -8,40 +8,33 @@ class Constraints {
   // @param {Object} schemaMap - map of req inputs => JOI schema to validate
   //  valid keys for schemaMap:  headers, body, query, params
   // @returns {Function} a middleware function to execute
-  static validateInputs(schemaMap) {
-    return celebrate(schemaMap);
+  static validateInputs(schemaMap, options) {
+    const opts = Object.assign({}, options);
+    return celebrate(schemaMap, opts);
   }
 
   // Facade wrapper for validateInputs
   // @param {Object} schemaObj - JOI schema to validate input against
-  static validateHeaders(schemaObj) {
-    return Constraints.validateInputs({
-      headers: schemaObj,
-    });
+  static validateHeaders(schemaObj, options) {
+    return Constraints.validateInputs({ headers: schemaObj }, options);
   }
 
   // Facade wrapper for validateInputs
   // @param {Object} schemaObj - JOI schema to validate input against
-  static validateBody(schemaObj) {
-    return Constraints.validateInputs({
-      body: schemaObj,
-    });
+  static validateBody(schemaObj, options) {
+    return Constraints.validateInputs({ body: schemaObj }, options);
   }
 
   // Facade wrapper for validateInputs
   // @param {Object} schemaObj - JOI schema to validate input against
-  static validateParams(schemaObj) {
-    return Constraints.validateInputs({
-      params: schemaObj,
-    });
+  static validateParams(schemaObj, options) {
+    return Constraints.validateInputs({ params: schemaObj }, options);
   }
 
   // Facade wrapper for validateInputs
   // @param {Object} schemaObj - JOI schema to validate input against
-  static validateQuery(schemaObj) {
-    return Constraints.validateInputs({
-      query: schemaObj,
-    });
+  static validateQuery(schemaObj, options) {
+    return Constraints.validateInputs({ query: schemaObj }, options);
   }
 }
 
