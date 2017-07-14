db5ae4ad5e6b1f47afb942628c7cc01c4c72e445
Naveed Nadjmabadi <naveedn@users.noreply.github.com>
2017-07-13 20:00:18 -0400

    re-format comments

--- constraints.js
+++ constraints.js
@@ -4,35 +4,45 @@ const celebrate = require('celebrate');
 
 class Constraints {
 
-  // Will validate the request object with the specified schemas
-  // @param {Object} schemaMap - map of req inputs => JOI schema to validate
-  //  valid keys for schemaMap:  headers, body, query, params
-  // @returns {Function} a middleware function to execute
+  /** Will validate the request object with the specified schemas
+   * @param {Object} schemaMap - map of req inputs => JOI schema to validate
+   * valid keys for schemaMap:  headers, body, query, params
+   * @param {Object} options - Joi options to be directly passed into validation
+   * @returns {Function} a middleware function to execute
+   */
   static validateInputs(schemaMap, options) {
     const opts = Object.assign({}, options);
     return celebrate(schemaMap, opts);
   }
 
-  // Facade wrapper for validateInputs
-  // @param {Object} schemaObj - JOI schema to validate input against
+  /** Facade wrapper for validateInputs
+   * @param {Object} schemaObj - JOI schema to validate input against
+   * @param {Object} options - [optional] options to pass to Joi directly
+   */
   static validateHeaders(schemaObj, options) {
     return Constraints.validateInputs({ headers: schemaObj }, options);
   }
 
-  // Facade wrapper for validateInputs
-  // @param {Object} schemaObj - JOI schema to validate input against
+  /** Facade wrapper for validateInputs
+   * @param {Object} schemaObj - JOI schema to validate input against
+   * @param {Object} options - [optional] options to pass to Joi directly
+   */
   static validateBody(schemaObj, options) {
     return Constraints.validateInputs({ body: schemaObj }, options);
   }
 
-  // Facade wrapper for validateInputs
-  // @param {Object} schemaObj - JOI schema to validate input against
+  /** Facade wrapper for validateInputs
+   * @param {Object} schemaObj - JOI schema to validate input against
+   * @param {Object} options - [optional] options to pass to Joi directly
+   */
   static validateParams(schemaObj, options) {
     return Constraints.validateInputs({ params: schemaObj }, options);
   }
 
-  // Facade wrapper for validateInputs
-  // @param {Object} schemaObj - JOI schema to validate input against
+  /** Facade wrapper for validateInputs
+   * @param {Object} schemaObj - JOI schema to validate input against
+   * @param {Object} options - [optional] options to pass to Joi directly
+   */
   static validateQuery(schemaObj, options) {
     return Constraints.validateInputs({ query: schemaObj }, options);
   }
